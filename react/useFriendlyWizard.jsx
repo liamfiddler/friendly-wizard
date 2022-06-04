4d1f7625de669e7b7ea6d8bf905f30117713f28b
Liam Fiddler <design@liamfiddler.com>
2022-06-04 11:09:39 +1000

    Add comments

--- react/useFriendlyWizard.jsx
+++ react/useFriendlyWizard.jsx
@@ -1,12 +1,15 @@
 import { useReducer } from 'react';
 import { Wizard } from '../index';
 
+// Using the Module Pattern to ensure only one wizard class is instantiated
 let wizard;
 
 const useFriendlyWizard = (wizardOptions) => {
   const [_, forceUpdate] = useReducer((x) => x + 1, 0);
 
   if (!wizard && !!wizardOptions) {
+    // React doesn't know when the wizard's internal values change, so
+    // we have to force a rerender whenever it changes to a different step
     class ReactWizard extends Wizard {
       next() {
         super.next();
